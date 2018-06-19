6157a0e9d5166fe9818489f8f73f8b79dae299a8
sunjinshuai <s_mike@163.com>
2018-06-19 15:43:41 +0800

    NSString+SafeKit

--- MYKit/SafeKit/FoundationContainer/NSString+SafeKit.m
+++ MYKit/SafeKit/FoundationContainer/NSString+SafeKit.m
@@ -24,6 +24,10 @@
         
         class_addMethod(kClass, @selector(substringWithRange:), (IMP)safeSubstringWithRange, method_getTypeEncoding(class_getInstanceMethod([self class], @selector(substringWithRange:))));
         
+        class_addMethod(kClass, @selector(substringFromIndex:), (IMP)safeSubstringFromOrToIndex, method_getTypeEncoding(class_getInstanceMethod([self class], @selector(substringFromIndex:))));
+        
+        class_addMethod(kClass, @selector(substringToIndex:), (IMP)safeSubstringFromOrToIndex, method_getTypeEncoding(class_getInstanceMethod([self class], @selector(substringToIndex:))));
+        
         objc_registerClassPair(kClass);
         self.isSafe = YES;
     }
@@ -58,12 +62,24 @@ void *safeSubstringWithRange(id self, SEL _cmd, NSRange range) {
         .super_class = class_getSuperclass(object_getClass(self))
     };
     void * (*objc_msgSendToSuper)(const void *, SEL, NSRange) = (void *)objc_msgSendSuper;
-    
     if (range.location + range.length > [self length]) {
-        
+        NSLog(@"\"%@\"-range:%@ must at range [0, %lu)", NSStringFromSelector(_cmd), NSStringFromRange(range), (unsigned long)[self length]);
         return @"";
     }
     return objc_msgSendToSuper(&superClass, _cmd, range);
+}
+
+id safeSubstringFromOrToIndex(id self, SEL _cmd, NSUInteger index) {
+    struct objc_super superClass = {
+        .receiver    = self,
+        .super_class = class_getSuperclass(object_getClass(self))
+    };
+    id (*objc_msgSendToSuper)(const void *, SEL, unsigned long) = (void *)objc_msgSendSuper;
+    if (index >= [self length]) {
+        NSLog(@"\"%@\" -index:(%lu) should less than %lu", NSStringFromSelector(_cmd), index, (unsigned long)[self length]);
+        return 0;
+    }
+    return objc_msgSendToSuper(&superClass, _cmd, index);
 }
 
 @end
--- MYKitDemo/ViewController.m
+++ MYKitDemo/ViewController.m
@@ -12,94 +12,91 @@
 
 @interface ViewController ()
 
-//@property (weak, nonatomic) IBOutlet UIImageView *imageView;
-//
-//@property (nonatomic, strong) NSArray *arrayI;
-//@property (nonatomic, strong) NSMutableArray *arrayM;
-//@property (nonatomic, strong) NSArray *array0;
-//@property (nonatomic, strong) NSArray *singleObjectArrayI;
-
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-  
-     /*
-        NSArray * array = @[@"A", @"B", @"C"].safe;
-    
-        [array objectAtIndex:0];
-        [array arrayByAddingObject:nil];
-        [array indexOfObject:@"B" inRange:NSMakeRange(0, 2)];
-        [array indexOfObjectIdenticalTo:@"B" inRange:NSMakeRange(0, 2)];
-        [array subarrayWithRange:NSMakeRange(0, 1)];
-        [array objectsAtIndexes:[NSIndexSet indexSetWithIndex:10]];
-        [array objectAtIndexedSubscript:1000];
-    
-   
-     [array arrayByAddingObjectsFromArray:nil];
-     [array componentsJoinedByString:nil];
-     [array containsObject:nil];
-     [array descriptionWithLocale:nil];
-     [array descriptionWithLocale:nil indent:1000];
-     [array firstObjectCommonWithArray:nil];
-     [array indexOfObject:nil];
-     [array isEqualToArray:nil];
-     [array indexOfObjectIdenticalTo:nil];
-     */
-    
-    //    NSMutableArray * mutableArray = [NSMutableArray new].safe;
-    //    [mutableArray addObject:@"AA"];
-    //    [mutableArray removeObjectAtIndex:5];
-    //    [mutableArray addObjectsFromArray:nil];
-    //    [mutableArray removeObject:nil inRange:NSMakeRange(0, 1)];
-    //    [mutableArray removeObject:nil];
-    //    [mutableArray removeObjectIdenticalTo:nil inRange:NSMakeRange(0, 1)];
-    //    [mutableArray removeObjectsInArray:nil];
-    //    [mutableArray setObject:nil atIndexedSubscript:0];
-    
-    //    NSSet * set = [NSSet set].safe;
-    //    [set anyObject];
-    //    [set containsObject:nil];
-    //    [set descriptionWithLocale:nil];
-    //    [set intersectsSet:nil];
-    //    [set isEqualToSet:nil];
-    //    [set isSubsetOfSet:nil];
-    //    [set setByAddingObject:nil];
-    //    id a= [set setByAddingObjectsFromSet:nil];
-    //    id b = [set setByAddingObjectsFromArray:nil];
-    
-    //    NSMutableSet * mutableSet = [NSMutableSet set].safe;
-    //    [set addObject:nil];
-    //    [mutableSet addObjectsFromArray:nil];
-    //    [mutableSet intersectSet:nil];
-    //    [mutableSet minusSet:nil];
     
     
-    //    NSPointerArray * pointerArray = [NSPointerArray weakObjectsPointerArray].safe;
-    //    [pointerArray pointerAtIndex:1000];
-    //    [pointerArray removePointerAtIndex:10000];
+//    NSArray * array = @[@"A", @"B", @"C"].safe;
+//
+//    [array objectAtIndex:0];
+//    [array arrayByAddingObject:nil];
+//    [array indexOfObject:@"B" inRange:NSMakeRange(0, 2)];
+//    [array indexOfObjectIdenticalTo:@"B" inRange:NSMakeRange(0, 2)];
+//    [array subarrayWithRange:NSMakeRange(0, 1)];
+//    [array objectsAtIndexes:[NSIndexSet indexSetWithIndex:10]];
+//    [array objectAtIndexedSubscript:1000];
+//
+//
+//    [array arrayByAddingObjectsFromArray:nil];
+//    [array componentsJoinedByString:nil];
+//    [array containsObject:nil];
+//    [array descriptionWithLocale:nil];
+//    [array descriptionWithLocale:nil indent:1000];
+//    [array firstObjectCommonWithArray:nil];
+//    [array indexOfObject:nil];
+//    [array isEqualToArray:nil];
+//    [array indexOfObjectIdenticalTo:nil];
+//
+    
+//    NSMutableArray * mutableArray = [NSMutableArray new].safe;
+//    [mutableArray addObject:@"AA"];
+//    [mutableArray removeObjectAtIndex:5];
+//    [mutableArray addObjectsFromArray:nil];
+//    [mutableArray removeObject:nil inRange:NSMakeRange(0, 1)];
+//    [mutableArray removeObject:nil];
+//    [mutableArray removeObjectIdenticalTo:nil inRange:NSMakeRange(0, 1)];
+//    [mutableArray removeObjectsInArray:nil];
+//    [mutableArray setObject:nil atIndexedSubscript:0];
+    
+//    NSSet * set = [NSSet set].safe;
+//    [set anyObject];
+//    [set containsObject:nil];
+//    [set descriptionWithLocale:nil];
+//    [set intersectsSet:nil];
+//    [set isEqualToSet:nil];
+//    [set isSubsetOfSet:nil];
+//    [set setByAddingObject:nil];
+//    id a = [set setByAddingObjectsFromSet:nil];
+//    id b = [set setByAddingObjectsFromArray:nil];
+
+//    NSMutableSet * mutableSet = [NSMutableSet set].safe;
+//    [mutableSet addObject:nil];
+//    [mutableSet addObjectsFromArray:nil];
+//    [mutableSet intersectSet:nil];
+//    [mutableSet minusSet:nil];
+//
+    
+//    NSPointerArray * pointerArray = [NSPointerArray weakObjectsPointerArray].safe;
+//    [pointerArray pointerAtIndex:1000];
+//    [pointerArray removePointerAtIndex:10000];
     
     
-    //    NSHashTable * hashTable = [NSHashTable hashTableWithOptions:(NSPointerFunctionsWeakMemory)];
-    //    NSLog(@"%@", hashTable);
-    //    [hashTable addObject:nil];
-    //    NSLog(@"%@", hashTable);
-    //    NSObject *obj = [NSObject new];
-    //    __weak NSObject *obj1 =obj;
-    //    [hashTable addObject:obj1];
-    //    NSLog(@"%@", hashTable);
-    //    [hashTable removeObject:[UIViewController new]];
-    
-    //    NSMapTable * mapTable = [NSMapTable weakToWeakObjectsMapTable];
-    //    id a = [mapTable objectForKey:nil];
-    //    [mapTable removeObjectForKey:nil];
-    //    [mapTable setObject:nil forKey:nil];
+//    NSHashTable * hashTable = [NSHashTable hashTableWithOptions:(NSPointerFunctionsWeakMemory)];
+//    NSLog(@"%@", hashTable);
+//    [hashTable addObject:nil];
+//    NSLog(@"%@", hashTable);
+//    NSObject *obj = [NSObject new];
+//    __weak NSObject *obj1 =obj;
+//    [hashTable addObject:obj1];
+//    NSLog(@"%@", hashTable);
+//    [hashTable removeObject:[UIViewController new]];
+//    
+//    NSMapTable * mapTable = [NSMapTable weakToWeakObjectsMapTable];
+//    id a = [mapTable objectForKey:nil];
+//    [mapTable removeObjectForKey:nil];
+//    [mapTable setObject:nil forKey:nil];
+//
     
     NSString *str = @"akdaldllad".safe;
 
+    [str substringFromIndex:200];
+    [str substringToIndex:200];
     [str characterAtIndex:213];
+    [str substringWithRange:NSMakeRange(0, 200)];
 }
 
 
