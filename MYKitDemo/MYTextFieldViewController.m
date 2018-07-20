5e0c4c9ef13c8ab3f326c49203b0889493874435
sunjinshuai <s_mike@163.com>
2018-07-20 11:30:54 +0800

    test TextField

--- MYKitDemo/MYTextFieldViewController.m
+++ MYKitDemo/MYTextFieldViewController.m
@@ -9,8 +9,9 @@
 #import "MYTextFieldViewController.h"
 #import "MYKitMacroHeader.h"
 #import "UITextField+Shake.h"
+#import "UITextField+InputLimit.h"
 
-@interface MYTextFieldViewController ()
+@interface MYTextFieldViewController ()<UITextFieldDelegate>
 
 @property (nonatomic, strong) UITextField *textField;
 
@@ -25,17 +26,19 @@
     
     UITextField *textField = [[UITextField alloc] init];
     textField.frame = CGRectMake((MYScreenWidth - 100)/2, 150, 100, 50);
+    textField.delegate = self;
+    textField.maxLength = 10;
     [textField.layer setBorderWidth:2];
     [textField.layer setBorderColor:[UIColor grayColor].CGColor];
     [self.view addSubview:textField];
     _textField = textField;
     
-    UIButton *actionSheetButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    actionSheetButton.frame = CGRectMake((MYScreenWidth - 150)/2, CGRectGetMaxY(textField.frame) + 50, 150, 50);
-    [actionSheetButton setTitle:@"showActionSheet" forState:UIControlStateNormal];
-    [actionSheetButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-    [actionSheetButton addTarget:self action:@selector(actionShake:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:actionSheetButton];
+    UIButton *shakeButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    shakeButton.frame = CGRectMake((MYScreenWidth - 150)/2, CGRectGetMaxY(textField.frame) + 50, 150, 50);
+    [shakeButton setTitle:@"showShakeTextField" forState:UIControlStateNormal];
+    [shakeButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [shakeButton addTarget:self action:@selector(actionShake:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:shakeButton];
 }
 
 - (IBAction)actionShake:(id)sender {
@@ -43,5 +46,17 @@
     [self.textField shake];
 }
 
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    NSUInteger length = textField.text.length + string.length;
+    
+    if (length > 10 && !(length == 1 && [string isEqualToString:@""])) {
+        [self.textField shake];
+        return NO;
+    } else {
+        return YES;
+    }
+    
+    return YES;
+}
 
 @end
