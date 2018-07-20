3f39960e30cd56b71dc792ae8899a9b00b384e27
sunjinshuai <s_mike@163.com>
2018-07-20 18:39:26 +0800

    UIButton 倒计时

--- MYKitDemo/MYButtonViewController.m
+++ MYKitDemo/MYButtonViewController.m
@@ -9,6 +9,7 @@
 #import "MYButtonViewController.h"
 #import "MYKitMacroHeader.h"
 #import "UIButton+CountDown.h"
+#import "UIButton+Indicator.h"
 
 @interface MYButtonViewController ()
 
@@ -27,12 +28,15 @@
 
 - (void)countDown {
     UIButton *countDownButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    countDownButton.backgroundColor = [UIColor orangeColor];
     countDownButton.frame = CGRectMake((MYScreenWidth - 150)/2, 84, 150, 50);
     [countDownButton setTitle:@"automaticWriting" forState:UIControlStateNormal];
     [countDownButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
     [countDownButton addTarget:self action:@selector(countDown:) forControlEvents:UIControlEventTouchUpInside];
+    [countDownButton showIndicator];
     [countDownButton scheduledTimerWithTimeInterval:10.0f countDownText:@"开始倒计时" completion:^(UIButton *countDownButton) {
         [countDownButton setTitle:@"倒计时完毕" forState:UIControlStateNormal];
+        [countDownButton hideIndicator];
     }];
     [self.view addSubview:countDownButton];
 }
