5dc590b93f2c80c37cbadb559c5766bbbd1eab27
sunjinshuai <s_mike@163.com>
2018-08-06 11:18:17 +0800

    KVO 防护

---
+++ MYKitDemo/UIKit/PlaceHolderView/MYPlaceHolderView.m
@@ -0,0 +1,21 @@
+//
+//  MYPlaceHolderView.m
+//  MYKitDemo
+//
+//  Created by QMMac on 2018/8/6.
+//  Copyright © 2018 com.51fanxing. All rights reserved.
+//
+
+#import "MYPlaceHolderView.h"
+
+@implementation MYPlaceHolderView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
